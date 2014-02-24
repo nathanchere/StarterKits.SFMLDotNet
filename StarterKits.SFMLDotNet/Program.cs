7bb0a5222d0a2a97f807e254a66495ed7e27ca1d
Nathan Chere <git@nathanchere.com.au>
2014-02-24 17:42:33 +1100

    Stable

---
+++ StarterKits.SFMLDotNet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace StarterKits.SFMLDotNet
+{
+    public static class Program
+    {        
+        private static void Main(string[] args)
+        {
+            var contextSettings = new ContextSettings { DepthBits = 32 };
+            var window = new RenderWindow(new VideoMode(640, 480), "SFML.Net starter kit", Styles.Default, contextSettings);
+            window.SetActive();
+
+            window.Closed += new EventHandler(OnClosed);
+            window.KeyPressed += new EventHandler<KeyEventArgs>(OnKeyPressed);
+
+            int r=0,g=0,b=0;
+            
+            while (window.IsOpen())
+            {
+                window.DispatchEvents();                
+                window.Clear(new Color((byte)r, (byte)g, (byte)b));
+                window.Display();
+            }
+        }
+
+        static void OnClosed(object sender, EventArgs e)
+        {
+            var window = (Window)sender;
+            window.Close();
+        }
+        
+        static void OnKeyPressed(object sender, KeyEventArgs e)
+        {
+            var window = (Window)sender;
+            if (e.Code == Keyboard.Key.Escape) window.Close();
+        }        
+        
+    }
+}
